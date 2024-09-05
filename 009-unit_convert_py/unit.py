2d5e275c8110cbbcc572de9ef3650698682aa5a7
SEANPNEX <ksgs1@163.com>
2024-09-04 22:19:57 -0400

    update 009 for type specification

--- 009-unit_convert_py/unit.py
+++ 009-unit_convert_py/unit.py
@@ -1,3 +1,4 @@
+from typing import Union
 CONVERT_DICT={
     "Kilogram": 1.0,
     "Pound": 0.453592,
@@ -7,7 +8,11 @@ CONVERT_DICT={
     "Gram": 0.001,
     "Oka": 1.2829
 }
-def convert_mass(value, origin_unit, result_unit):
+def convert_mass(value: Union[float, int], origin_unit, result_unit):
+    '''
+    The function uses a dictionary to convert the input value from origin unit to desired unit
+    (value: in, origin_unit: str, result_unit: str) -> result: int
+    '''
     convert_param = CONVERT_DICT.get(origin_unit) / CONVERT_DICT.get(result_unit)
     result = float(value * convert_param)
     return result
