aa7fcb174a379427be87a7204d5390de3be72955
sean <ksgs1@163.com>
2024-09-04 20:54:19 -0400

    update ans for 007

---
+++ 007-paycheck_py/paycheck.py
@@ -0,0 +1,12 @@
+TAXRATE=0.2
+hours=input("How many hours did employee work?")
+payrate=input("What is the employees pay rate?")
+try:
+    hours=float(hours)
+    payrate=float(payrate)
+except:
+    raise TypeError("Input should be a number")
+totalPay=min(hours,40)*payrate+1.5*payrate*max(0,hours-40)
+taxes=totalPay*TAXRATE
+netPay=totalPay*(1-TAXRATE)
+print(f"Total pay: {totalPay}\nTaxes: {taxes}\n Net pay: {netPay}")
