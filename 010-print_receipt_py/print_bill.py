854bebc311adaa58828269656c09c1b36d5a6739
SEANPNEX <ksgs1@163.com>
2024-09-04 23:13:46 -0400

    update 010

--- 010-print_receipt_py/print_bill.py
+++ 010-print_receipt_py/print_bill.py
@@ -1,2 +1,13 @@
 def print_receipt(subtotal, tax_rate, tip_rate):
-    
+    """
+    This function takes subtotal: float, tax_rate: float, tip_rate: float and prints the bill
+    (subtotal: float, tax_rate: float, tip_rate: float) -> Null
+    """
+    format=f'''
+    {"Subtotal:":>9} ${subtotal:>10.2f}
+    {"Tax:":>9} ${tax_rate * subtotal:>10.2f}
+    {"Tip:":>9} ${tip_rate * subtotal:>10.2f}
+    {"==========":>21}
+    {"Total:":>9} ${subtotal*(1+tax_rate+tip_rate):>10.2f}
+    '''
+    print(format)
