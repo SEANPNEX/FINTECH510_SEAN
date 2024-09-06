b674517e5c273e5c6787dddeb472b4beff912660
SEANPNEX <ksgs1@163.com>
2024-09-06 10:14:48 -0400

    some updates

--- 010-print_receipt_py/print_bill.py
+++ 010-print_receipt_py/print_bill.py
@@ -1,13 +1,15 @@
+
 def print_receipt(subtotal, tax_rate, tip_rate):
     """
     This function takes subtotal: float, tax_rate: float, tip_rate: float and prints the bill
     (subtotal: float, tax_rate: float, tip_rate: float) -> Null
     """
-    format=f'''
-    {"Subtotal:":>9} ${subtotal:>10.2f}
-    {"Tax:":>9} ${tax_rate * subtotal:>10.2f}
-    {"Tip:":>9} ${tip_rate * subtotal:>10.2f}
-    {"==========":>21}
-    {"Total:":>9} ${subtotal*(1+tax_rate+tip_rate):>10.2f}
-    '''
-    print(format)
+    
+    print(f"""{"Subtotal:":>9} ${subtotal:>10.2f}""")
+    print(f"""{"Tax:":>9} ${tax_rate * subtotal:>10.2f}""")
+    print(f"""{"Tip:":>9} ${tip_rate * subtotal:>10.2f}""")
+    print(f"""{"==========":>21}""")
+    print(f"""{"Total:":>9} ${subtotal*(1 + tax_rate + tip_rate):>10.2f}""")
+
+
+
--- 010-print_receipt_py/test.py
+++ 010-print_receipt_py/test.py
@@ -1,3 +1,5 @@
 from print_bill import print_receipt
 
 print_receipt(10.00, .05, .2)
+
+print_receipt(0, .05, .2)
